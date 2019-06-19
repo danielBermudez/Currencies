879387aa6b256ba86ad07d1e286e4613e0847338
Andrés Mendieta <carlos.camyoh@gmail.com>
2019-06-19 15:43:56 -0500

    saving trips with persistence

--- currenciesPrototype/currenciesPrototype/ViewController.swift
+++ currenciesPrototype/currenciesPrototype/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func AddTripTouched(_ sender: Any) {
+        addTrip()
+        print("\n\(trips.count)   ------------------------------\n")
+        for trip in trips {
+            print(trip.currencyType)
+        }
+    }
+    
+    @IBAction func DeleteTripsTouched(_ sender: Any) {
+        deleteTrips()
+    }
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var trips = [Trip]()
+    private var filtered = [Trip]()
+    private var isFiltered = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        do {
+            trips = try context.fetch(Trip.fetchRequest())
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    private func addTrip() {
+        let trip = Trip(entity: Trip.entity(), insertInto: context)
+        trip.currencyType = "USD"
+        appDelegate.saveContext()
+        trips.append(trip)
+        
+    }
+    
+    private func deleteTrips(){
+        for object in trips {
+            context.delete(object)
+        }
+    }
 }
 
