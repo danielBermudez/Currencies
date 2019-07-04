658d1970a32787ed36cc1781f6ce32ac549108b2
Andrés Mendieta <carlos.camyoh@gmail.com>
2019-07-04 09:21:11 -0500

    hiding keyboard and pickerView

--- currenciesPrototype/currenciesPrototype/Modules/Trips/TripViewController.swift
+++ currenciesPrototype/currenciesPrototype/Modules/Trips/TripViewController.swift
@@ -21,26 +21,46 @@ class TripViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak private var currencyPicker: UIPickerView!
     @IBOutlet weak private var currencyRate: UITextField!
     @IBOutlet weak private var CurrencyRateHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pickerConstraint: NSLayoutConstraint!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        countryViewModel.saveData()
+        countryViewModel.saveData()
         configureTextField()
         configurePickerView()
-        
+        configureTextFieldTargets()
+        hidePickerView()
+        dismissKey()
     }
+    
     private func configureTextField() {
         destinationSearchField.delegate = self
         originSearchField.delegate = self
         currencyField.delegate = self
         
     }
+    
+    private func configureTextFieldTargets(){
+        currencyField.addTarget(self, action: #selector(showPickerView), for: .editingDidBegin)
+        currencyField.addTarget(self, action: #selector(hidePickerView), for: .editingDidEnd)
+    }
+    
     private func configurePickerView() {
         currencyPicker.delegate = self
         currencyPicker.dataSource = self
     }
-     func textFieldDidBeginEditing(_ textField: UITextField) {
+    
+    @objc private func showPickerView(){
+        pickerConstraint.constant = 216
+    }
+    
+    @objc private func hidePickerView(){
+        pickerConstraint.constant = 0
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField == originSearchField {
             performSegue(withIdentifier: "showOriginSearchView", sender: self)
         } else if textField == destinationSearchField{
@@ -105,3 +125,17 @@ extension TripViewController : CountrySelectionDelegate {
     }
 }
 
+//MARK: - Keyboard Settings
+
+extension UIViewController {
+    func dismissKey()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer( target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+}
